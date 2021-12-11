8386e380961dda5f1f0cffedf450d8791aeadc2c
Ayoub Rezaei <rez4eii@gmail.com>
2021-12-11 11:53:40 +0330

    Poduct component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All right is reserved</footer>
-    </div>
-  );
+import Products from "./Components/Products";
+import data from "./data.json";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart items</div>
+          </div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
