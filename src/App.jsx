a2229dd726cc6efd10ebe64f2d61142e7078ea96
Ayoub Rezaei <rez4eii@gmail.com>
2021-12-11 10:12:42 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
